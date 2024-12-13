ae96559d5d7b0cc2eaf3687f5534ace5bbdc2ccc
qiuxan <qiuxan@gmail.com>
2024-12-13 17:26:16 +1100

    Add Dependency Injection file in .infrastructure project and use it in the API.

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -1,4 +1,11 @@
+using eCommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//add services to the
+
+builder.Services.AddInfrastructure();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
