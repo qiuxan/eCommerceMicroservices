36274f77c66eff10e0c3c64930312d45b43c1144
qiuxan <qiuxan@gmail.com>
2024-12-13 17:47:11 +1100

    add core service DI file and use

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -1,3 +1,4 @@
+using eCommerce.Core;
 using eCommerce.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 //add services to the
 
 builder.Services.AddInfrastructure();
+builder.Services.AddCore();
 
 var app = builder.Build();
 
---
+++ eCommerceSolution.UsersService/eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace eCommerce.Core;
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Extension method to add core services to the DI container
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection
+        AddCore(this IServiceCollection services)
+    {
+        //TODO: Add services to the IoC container
+
+        //Core services often include data access, caching and other low-level components
+
+        return services;
+    }
+}
