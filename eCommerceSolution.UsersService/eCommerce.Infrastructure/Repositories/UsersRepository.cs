d554501657734e36c350ea7c65eadf64d3e465f3
qiuxan <qiuxan@gmail.com>
2024-12-20 16:04:30 +1100

    set query for GetUserByEmailAndPassword function for user Repository

--- eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -42,13 +42,14 @@ internal class UsersRepository : IUsersRepository
 
     public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
     {
-        return new ApplicationUser
-        {
-            UserId = Guid.NewGuid(),
-            Email = email,
-            Password = password,
-            PersonName = "John Doe",
-            Gender = GenderOptions.Male.ToString()
-        };
+
+        //SQL query to retrieve the user from the database "Users" table
+
+        string query = "SELECT * FROM public.\"Users\" WHERE \"Email\" = @Email AND \"Password\" = @Password";
+        var parameters = new { Email = email, Password = password };
+        ApplicationUser? user = await
+        _dbContext.DbConnection.QueryFirstOrDefaultAsync<ApplicationUser>(query, parameters);
+
+        return user; // firstOrDefault will return null if no user found so no need to check for null
     }
 }
