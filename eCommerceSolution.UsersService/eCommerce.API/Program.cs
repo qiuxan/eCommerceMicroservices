5a8dae517ef322ee12d8cd80efc8b5672da538a0
qiuxan <qiuxan@gmail.com>
2024-12-21 16:12:04 +1100

    Setup The Swagger and Cors

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -24,12 +24,38 @@ builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 //FluentValidations
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add API explorer services
+builder.Services.AddEndpointsApiExplorer();
+
+//Add Swagger generator services to create Swagger specifications
+
+builder.Services.AddSwaggerGen();
+
+//Add Cors services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") //Allow requests from the Angular app
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 
 //Routing
 app.UseRouting();
+
+app.UseSwagger(); //Add endpoint to serve generated Swagger as a JSON endpoint
+
+app.UseSwaggerUI();// Add swagger UI (interactive page to explore and test API endpoints)
+
+app.UseCors();
 
 //Auth
 app.UseAuthentication();
