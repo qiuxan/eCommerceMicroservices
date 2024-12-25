1e9215a385f14d96ed8352f4df33b01fd77cae58
qiuxan <qiuxan@gmail.com>
2024-12-26 07:50:34 +1100

    Products Fluent Validation(No need to DI every single validator because there is an auto DI in program.cs)

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+public class ProductAddRequestValidator:AbstractValidator<ProductAddRequest>
+{
+    public ProductAddRequestValidator()
+    {
+        RuleFor(p=>p.ProductName)
+            .NotEmpty().WithMessage("Product Name can't be blank");
+
+        RuleFor(p => p.Category)
+            .IsInEnum();
+
+        RuleFor(p => p.UnitPrice)
+            .InclusiveBetween(0,double.MaxValue).WithMessage($"Unit Price must be between 0 and {double.MaxValue}");
+
+        RuleFor(p => p.QuantityInstock)
+            .InclusiveBetween(0, int.MaxValue).WithMessage($"Unit Price must be between 0 and {int.MaxValue}");
+
+    }
+}
---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        RuleFor(p => p.ProductID)
+            .NotEmpty().WithMessage("Product ID can't be blank");
+
+        RuleFor(p => p.ProductName)
+            .NotEmpty().WithMessage("Product Name can't be blank");
+
+        RuleFor(p => p.Category)
+            .IsInEnum();
+
+        RuleFor(p => p.UnitPrice)
+            .InclusiveBetween(0, double.MaxValue).WithMessage($"Unit Price must be between 0 and {double.MaxValue}");
+
+        RuleFor(p => p.QuantityInstock)
+            .InclusiveBetween(0, int.MaxValue).WithMessage($"Unit Price must be between 0 and {int.MaxValue}");
+
+    }
+}
