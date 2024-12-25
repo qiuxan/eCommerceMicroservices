00052f66b231a383d103310187a4a9345e2dd687
qiuxan <qiuxan@gmail.com>
2024-12-25 18:51:35 +1100

    Products Repository and I repository

---
+++ eCommerceSolution.ProductsService/DataAccessLayer/Repositories/ProductsRepository.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using DataAccessLayer.Entities;
+using eCommerce.DataAccessLayer.Context;
+using eCommerce.DataAccessLayer.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.DataAccessLayer.Repositories;
+public class ProductsRepository: IProductsRepository
+{
+    private readonly ApplicationDbContext _DbContext;
+
+    public ProductsRepository(ApplicationDbContext DbContext)
+    {
+        _DbContext = DbContext;
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts()
+    {
+        return await _DbContext.Products.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product?>> GetProductsByCondition(Expression<Func<Product, bool>> conditionExpression)
+    {
+        return await _DbContext.Products.Where(conditionExpression).ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression)
+    {
+        return await _DbContext.Products.FirstOrDefaultAsync(conditionExpression);
+    }
+
+    public async Task<Product?> AddProduct(Product product)
+    {
+        _DbContext.Products.Add(product);
+        await _DbContext.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product?> UpdateProduct(Product product)
+    {
+        Product? existingProduct = await _DbContext.Products.FirstOrDefaultAsync(p => p.ProductID == product.ProductID);
+
+        if (existingProduct == null)
+        {
+            return null;
+        }
+
+        existingProduct.ProductName = product.ProductName;
+        existingProduct.Category = product.Category;
+        existingProduct.UnitPrice = product.UnitPrice;
+        existingProduct.QuantityInStock = product.QuantityInStock;
+
+        await _DbContext.SaveChangesAsync();
+
+        return product;
+    }
+
+    public async Task<bool> DeleteProduct(Guid productID)
+    {
+        Product? existintProductroduct = await _DbContext.Products.FirstOrDefaultAsync(p => p.ProductID == productID);
+        if (existintProductroduct == null)
+        {
+            return false;
+        }
+        _DbContext.Products.Remove(existintProductroduct);
+
+        int rowsAffected = await _DbContext.SaveChangesAsync();
+
+        return rowsAffected>0;
+    }
+}
---
+++ eCommerceSolution.ProductsService/DataAccessLayer/RepositoryContracts/IProductsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+using DataAccessLayer.Entities;
+
+namespace eCommerce.DataAccessLayer.RepositoryContracts;
+/// <summary>
+/// Represents a repository for managing products table in the database
+/// </summary>
+public interface IProductsRepository
+{
+    /// <summary>
+    /// retrieves all products asynchronously
+    /// </summary>
+    /// <returns> returns all products from the table</returns>
+    Task<IEnumerable<Product>> GetProducts();
+
+    /// <summary>
+    /// retrieves all products asynchronously based on specified condition.
+    /// </summary>
+    /// <param name="conditionExpression">The condition to filter the products in linq</param>
+    /// <returns>Return a collection of matching products</returns>
+    Task<IEnumerable<Product?>> GetProductsByCondition(Expression<Func<Product,bool>> conditionExpression);
+
+    /// <summary>
+    /// retrieves a single asynchronously based on specified condition.
+    /// </summary>
+    /// <param name="conditionExpression">The condition to filter the product in linq</param>
+    /// <returns>Return a matching product or null</returns>
+    Task<Product?> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression);
+
+
+    /// <summary>
+    /// add a product to the database asynchronously
+    /// </summary>
+    /// <param name="product">the product to be added</param>
+    /// <returns>returns the added product object or null if unsuccessful</returns>
+    Task<Product?> AddProduct(Product product);
+
+
+    /// <summary>
+    /// update a product to the database asynchronously
+    /// </summary>
+    /// <param name="product">the product to be updated</param>
+    /// <returns>returns the updated product object or null if unsuccessful</returns>
+    Task<Product?> UpdateProduct(Product product);
+
+    /// <summary>
+    /// delete a product from the database asynchronously
+    /// </summary>
+    /// <param name="productID">The product ID to be deleted</param>
+    /// <returns>Returns true if the deletion is successful, false otherwise</returns>
+    Task<bool> DeleteProduct(Guid productID);
+}
